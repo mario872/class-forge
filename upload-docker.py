46a6dcffd9780ec9256485c933fc89d63909f7e3
mario872 <106939571+mario872@users.noreply.github.com>
2024-03-09 09:45:44 +1100

    Updated docker build / Deleted empty playwright tests

--- upload-docker.py
+++ upload-docker.py
@@ -1,5 +1,5 @@
 import subprocess
-docker_v = 0.7
+docker_v = 0.8
 
 commands = [f'sudo docker build . -t solderingiron86/better-sentral:{docker_v}',
             f'sudo docker push solderingiron86/better-sentral:{docker_v}']
