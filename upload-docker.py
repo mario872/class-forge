46b328704cfe2746283b9500b69f6daaab45f706
mario872 <106939571+mario872@users.noreply.github.com>
2024-03-16 15:38:11 +1100

    Finished uploading docker v0.15, and added testing to upload_docker.py

--- upload-docker.py
+++ upload-docker.py
@@ -2,7 +2,9 @@ import subprocess
 docker_v = 0.15
 
 commands = [f'sudo docker build . -t solderingiron86/better-sentral:{docker_v}',
-            f'sudo docker push solderingiron86/better-sentral:{docker_v}']
+            f'sudo docker run --name better-sentral-{docker_v} -p 5000:5000 solderingiron86/better-sentral:{docker_v}'
+            f'sudo docker push solderingiron86/better-sentral:{docker_v}',
+            f'sudo docker rm solderingiron86/better-sentral:{docker_v}']
 
 
 for command in commands:
